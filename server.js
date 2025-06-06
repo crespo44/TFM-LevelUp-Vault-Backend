603f31ca81e10626b24a3a13b3dc2ff5f1d0578c
Jorge Crespo de Troya <crespo_914@hotmail.com>
2025-06-06 13:09:06 +0200

    Ultimos retoques y añadido start node server en package

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const app = require('./src/app');
 require('dotenv').config();
 const connectDB = require('./src/config/database');
 
-//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
     try {
         
         await connectDB();
 
-        /*app.listen(PORT, () => {
+        app.listen(PORT, () => {
             console.log(`Servidor corriendo en ${PORT}`);
-        });*/
+        });
 
     } catch(error) {
         console.log('No se ha podido levantar el servidor', error);
